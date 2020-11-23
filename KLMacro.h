ce20e9e4c1f95a854d05be2a6ec4627d468878a9
kaili.lu <kaili.lu@happy-seed.com>
2020-11-23 18:28:32 +0800

    fir commit

---
+++ KLMacro.h
@@ -0,0 +1,16 @@
+//
+//  KLMacro.h
+//  LMacro
+//
+//  Created by captain on 2020/11/23.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KLMacro : NSObject
+
+@end
+
---
+++ KLMacro.m
@@ -0,0 +1,12 @@
+//
+//  KLMacro.m
+//  LMacro
+//
+//  Created by captain on 2020/11/23.
+//
+
+#import "KLMacro.h"
+
+@implementation KLMacro
+
+@end
